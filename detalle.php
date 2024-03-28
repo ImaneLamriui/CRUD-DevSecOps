56f3a5bd42c5ef99825a602266a19a888fdf24c1
Imane Lamriui <121883455+ImaneLamriui@users.noreply.github.com>
2024-03-28 22:19:00 +0100

    sanitization

--- detalle.php
+++ detalle.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>DWES03_TAREA_FICHERO_crear.php</title>
+    <title>detalle.php</title>
 </head>
 
-<body class="card text-bg-info"   style="border:none;"> <!--podemos remplazar esta clase con : style="background:aqua;"-->
+<body class="card text-bg-info" style="border:none;"> <!--podemos remplazar esta clase con : style="background:aqua;"-->
 
 
     <div>
@@ -23,26 +23,30 @@
         require_once 'conexion.php';
 
         if ($_GET) {
+            // utilizar consultas preparadas con marcadores de posición para protegerse de la Inyección SQL
+            //$id = $_GET['id'];
+            //Sanitización de Datos, asegurar de que los datos mostrados en el formulario estén debidamente escapados para prevenir posibles ataques XSS
+            $id = htmlspecialchars($_GET['id']);
 
-            $id = $_GET['id'];
+            $sql = $conexion->prepare("SELECT nombre,nombre_corto,descripcion,pvp,familia  FROM productos WHERE id = ?");
+            $sql->execute([$id]);
 
-            $sql = $conexion->prepare("SELECT nombre,nombre_corto,descripcion,pvp,familia  FROM productos WHERE id = $id");
-            $sql->execute();
+            $detalles1 = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-            $detalles1 = $sql->fetchAll();
-
-            $consulta3 = $conexion->prepare("select cod, nombre from familias WHERE cod= $id");
-            $consulta3->execute();
-            $detalles2 = $consulta3->fetchAll();
+            // $consulta3 = $conexion->prepare("select cod, nombre from familias WHERE cod= $id");
+            // $consulta3->execute();
+            // $detalles2 = $consulta3->fetchAll();
 
             foreach ($detalles1 as $detalle) {
 
                 echo '<br>';
-                echo ("<h4>{$detalle['nombre']}<br></h4>");
+                // echo ("<h4>{$detalle['nombre']}<br></h4>");
+                echo ("<h4>" . htmlspecialchars($detalle['nombre']) . "<br></h4>");
+
                 echo ("<h5>Código: $id<br></h5>");
-                echo ("<i style='font-style:normal;font-weight: bold;color: white;'>Nombre </i>: " . $detalle['nombre']. '<br>');
+                echo ("<i style='font-style:normal;font-weight: bold;color: white;'>Nombre </i>: " . $detalle['nombre'] . '<br>');
                 echo ("<i style='font-style:normal;font-weight: bold;color: white;'>Nombre corto </i>: " . $detalle['nombre_corto'] . '<br>');
-                echo ("<i style='font-style:normal;font-weight: bold;color: white;'>PVP </i>: " . $detalle['pvp'].'€' . '<br>');
+                echo ("<i style='font-style:normal;font-weight: bold;color: white;'>PVP </i>: " . $detalle['pvp'] . '€' . '<br>');
                 echo ("<i style='font-style:normal;font-weight: bold;color: white;'>Código familia </i>: " . $detalle['familia'] . '<br>');
                 echo ("<i style='font-style:normal;font-weight: bold;color: white;'>Descripcion </i>: " . $detalle['descripcion'] . '<br>');
             }
