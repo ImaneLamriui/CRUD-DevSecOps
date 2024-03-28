e1e9d15a9b5b13de7a5c58116c56b3249c17f266
Imane Lamriui <121883455+ImaneLamriui@users.noreply.github.com>
2024-03-28 22:17:38 +0100

    updated

--- borrar.php
+++ borrar.php
@@ -2,9 +2,12 @@
 require_once 'conexion.php';
 //Obtenerle con GET o REQUEST
 if ($_GET) {
-  $id = $_GET['id'];
-  $sql = $conexion->prepare("DELETE FROM productos WHERE id= $id ");
-  if ($sql->execute()) {
+  //Utilizar consultas parametrizadas
+  //Sanitización de los datos para evitar los ataques XSS
+  $id = htmlspecialchars($_GET['id']);
+  //$id = $_GET['id'];
+  $sql = $conexion->prepare("DELETE FROM productos WHERE id = ? ");
+  if ($sql->execute([$id])) {
     print("producto con código: $id Borrado correctamente");
   } else {
     print("Fallido");
