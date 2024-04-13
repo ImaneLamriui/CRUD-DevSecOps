b3429a8f008824d8373af4172cdced9ecc33b0b9
Imane Lamriui <121883455+ImaneLamriui@users.noreply.github.com>
2024-04-13 12:31:37 +0200

    updated

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -11,12 +11,12 @@ if (isset($_SESSION['nombre_usuario'])) {
         // Destruir la sesión
         session_destroy();
         // Redirigir al usuario a la página de inicio de sesión u otra página deseada
-        header("Location: iniciar_sesion.php");
+        header("Location: index.php");
         exit();
     }
 } else {
     // Si el usuario no ha iniciado sesión, redirigirlo a la página de inicio de sesión
-    header("Location: iniciar_sesion.php");
+    header("Location: index.php");
     exit();
 }
 ?>
