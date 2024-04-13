b8a0d8731b5d83c7b5afb23898ffbae816c670fb
Imane Lamriui <121883455+ImaneLamriui@users.noreply.github.com>
2024-04-13 16:48:05 +0200

    updated

--- crear.php
+++ crear.php
@@ -75,37 +75,30 @@
           </div>
 
           <?php
-          require_once 'conexion.php';
-          // evitar la ejecución del código de inserción si no se ha enviado el formulario.
-          // Sanitización del código para prevenir ataques XSS and parameterized queries
-          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+require_once 'conexion.php';
 
-            $nombre = htmlspecialchars($_POST['nombre']);
-            $nombre_corto = htmlspecialchars($_POST['nombre_corto']);
-            $descripcion = htmlspecialchars($_POST['descripcion']);
-            $pvp = htmlspecialchars($_POST['pvp']);
-            $familia = htmlspecialchars($_POST['familia']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $nombre_corto = htmlspecialchars($_POST['nombre_corto']);
+    $descripcion = htmlspecialchars($_POST['descripcion']);
+    $pvp = htmlspecialchars($_POST['pvp']);
+    $familia = htmlspecialchars($_POST['familia']);
 
-            $consulta = $conexion->prepare("INSERT INTO productos (nombre, nombre_corto, descripcion, pvp, familia) VALUES (?, ?, ?, ?, ?)");
-            $consulta->execute([$nombre, $nombre_corto, $descripcion, $pvp, $familia]);
+    $consulta = $conexion->prepare("INSERT INTO productos (nombre, nombre_corto, descripcion, pvp, familia) VALUES (?, ?, ?, ?, ?)");
+    $consulta->execute([$nombre, $nombre_corto, $descripcion, $pvp, $familia]);
 
-            if ($consulta->rowCount() > 0) {
-              echo "Registro agregado correctamente.";
-            } else {
-              echo "Error al agregar el registro.";
-            }
-            // Ejecutar la consulta con los valores correspondientes
-            if ($consulta->execute()) {
-            // Si la consulta se ejecuta correctamente
-              $_SESSION['mensaje'] = "Producto creado correctamente.";
-              echo "<script>alert('Producto creado correctamente.'); window.location.href='listado.php';</script>";
-            } else {
-              // Si la consulta falla
-              $_SESSION['mensaje'] = "Error al crear el producto.";
-              echo "<script>alert('Error al crear el producto.'); window.location.href='listado.php';</script>";
-            }
-          }
-          ?>
+    if ($consulta->rowCount() > 0) {
+        echo "Registro agregado correctamente.";
+        $_SESSION['mensaje'] = "Producto creado correctamente.";
+        echo "<script>alert('Producto creado correctamente.'); window.location.href='listado.php';</script>";
+    } else {
+        echo "Error al agregar el registro.";
+        $_SESSION['mensaje'] = "Error al crear el producto.";
+        echo "<script>alert('Error al crear el producto.'); window.location.href='listado.php';</script>";
+    }
+}
+?>
+
     </form>
 
   </div>
